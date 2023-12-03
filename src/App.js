90f336a5132655ed070129aeb81a528b259d05ca
Sumana Sattar <sumana.sattar@coop.se>
2023-12-03 06:59:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+     <div className='Image-wrapper'>
+      pasta
+     </div>
+    </header>
+    <div className='Hero'>
+      <h1>PASTA</h1>
+      <p>Coming Soon...,</p>
     </div>
+    
+  </div>
   );
 }
 
